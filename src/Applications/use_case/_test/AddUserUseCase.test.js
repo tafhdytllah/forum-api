9bb7b4581d2c381bf0b0d1a1d86c12076ace74f8
Taufik Hidayatullah <taufik@fungsitama.com>
2025-06-04 22:01:14 +0700

    use_case: add use case user and auth

---
+++ src/Applications/use_case/_test/AddUserUseCase.test.js
@@ -0,0 +1,61 @@
+import AddUserUseCase from '../AddUserUseCase';
+import UserRepository from '../../../Domains/users/UserRepository';
+import PasswordHash from '../../security/PasswordHash';
+import RegisterUser from '../../../Domains/users/entities/RegisterUser';
+import RegisteredUser from '../../../Domains/users/entities/RegisteredUser';
+
+describe('AddUserUseCase', () => {
+  /**
+   * Menguji apakah use case mampu mengoskestrasikan langkah demi langkah dengan benar.
+   */
+  it('should orchestrating the add user action correctly', async () => {
+    // Arrange
+    const useCasePayload = {
+      username: 'dicoding',
+      password: 'secret',
+      fullname: 'Dicoding Indonesia',
+    };
+
+    const mockRegisteredUser = new RegisteredUser({
+      id: 'user-123',
+      username: useCasePayload.username,
+      fullname: useCasePayload.fullname,
+    });
+
+    /** creating dependency of use case */
+    const mockUserRepository = new UserRepository();
+    const mockPasswordHash = new PasswordHash();
+
+    /** mocking needed function */
+    mockUserRepository.verifyAvailableUsername = jest.fn()
+      .mockImplementation(() => Promise.resolve());
+    mockPasswordHash.hash = jest.fn()
+      .mockImplementation(() => Promise.resolve('encrypted_password'));
+    mockUserRepository.addUser = jest.fn()
+      .mockImplementation(() => Promise.resolve(mockRegisteredUser));
+
+    /** creating use case instance */
+    const getUserUseCase = new AddUserUseCase({
+      userRepository: mockUserRepository,
+      passwordHash: mockPasswordHash,
+    });
+
+    // Action
+    const registeredUser = await getUserUseCase.execute(useCasePayload);
+
+    // Assert
+    expect(registeredUser).toStrictEqual(new RegisteredUser({
+      id: 'user-123',
+      username: useCasePayload.username,
+      fullname: useCasePayload.fullname,
+    }));
+
+    expect(mockUserRepository.verifyAvailableUsername).toHaveBeenCalledWith(useCasePayload.username);
+    expect(mockPasswordHash.hash).toHaveBeenCalledWith(useCasePayload.password);
+    expect(mockUserRepository.addUser).toHaveBeenCalledWith(new RegisterUser({
+      username: useCasePayload.username,
+      password: 'encrypted_password',
+      fullname: useCasePayload.fullname,
+    }));
+  });
+});

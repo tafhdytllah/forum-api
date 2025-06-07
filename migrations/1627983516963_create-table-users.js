e9948a125ad41766c4d47f1fcb0b5b488c5ba686
Taufik Hidayatullah <taufik@fungsitama.com>
2025-06-07 20:20:34 +0700

    project starter

--- migrations/1627983516963_create-table-users.js
+++ migrations/1627983516963_create-table-users.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 exports.up = (pgm) => {
   pgm.createTable('users', {
     id: { type: 'VARCHAR(50)', primaryKey: true },
--- migrations/1627983555473_create-table-authentications.js
+++ migrations/1627983555473_create-table-authentications.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 exports.up = (pgm) => {
   pgm.createTable('authentications', {
     token: { type: 'TEXT', notNull: true },

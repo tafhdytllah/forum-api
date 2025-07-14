045b9c54a83132d2623cad1a051e7b02600f6fac
Taufik Hidayatullah <taufik@fungsitama.com>
2025-07-14 23:48:13 +0700

    restore is_deleted column

--- migrations/1752463771910_create-table-user-comment-likes.js
+++ migrations/1752463771910_create-table-user-comment-likes.js
@@ -3,6 +3,7 @@ exports.up = (pgm) => {
     id: { type: 'VARCHAR(50)', primaryKey: true },
     user_id: { type: 'VARCHAR(50)', notNull: true, references: 'users(id)', onDelete: 'CASCADE' },
     comment_id: { type: 'VARCHAR(50)', notNull: true, references: 'comments(id)', onDelete: 'CASCADE' },
+    is_deleted: { type: 'BOOLEAN', notNull: true, default: false },
     created_at: { type: "TEXT", notNull: true},
     updated_at: { type: "TEXT", notNull: true},
   });
